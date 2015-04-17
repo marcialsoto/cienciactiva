8932ed04bcaabf03424b880e7d3d3b8f07a76c96
Marcial Soto <hola@marcialsoto.com>
2015-04-17 15:21:12 -0500

    Usuario cambiado

--- configuration.php
+++ configuration.php
@@ -5,7 +5,7 @@ class JConfig {
 	public $display_offline_message = '1';
 	public $offline_image = '';
 	public $sitename = 'CIENCIACTIVA';
-	public $editor = 'codemirror';
+	public $editor = 'tinymce';
 	public $captcha = 'recaptcha';
 	public $list_limit = '20';
 	public $access = '1';
